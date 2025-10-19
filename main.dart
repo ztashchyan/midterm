6a2e1b22534b0f37f465c00ffb92dfe6e3fce60d
ztashchyan <162495513+ztashchyan@users.noreply.github.com>
2025-10-19 14:44:33 +0400

    Format main.dart with consistent spacing

--- main.dart
+++ main.dart
@@ -2,25 +2,31 @@ import 'package:flutter/material.dart';
 import 'homework_bloc.dart';
 import 'homework_list_screen.dart';
 
+
 void main() {
   runApp(const HomeworkApp());
 }
 
+
 class HomeworkApp extends StatefulWidget {
   const HomeworkApp({Key? key}) : super(key: key);
+
 
   @override
   State<HomeworkApp> createState() => _HomeworkAppState();
 }
 
+
 class _HomeworkAppState extends State<HomeworkApp> {
   final HomeworkBloc _bloc = HomeworkBloc();
+
 
   @override
   void dispose() {
     _bloc.dispose();
     super.dispose();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,4 +37,5 @@ class _HomeworkAppState extends State<HomeworkApp> {
     );
   }
 }
+
 
